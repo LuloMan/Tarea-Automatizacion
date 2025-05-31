4c1b47cf0a37bedafb4758182c28681cadbb14fc
LuloMan <lucas.997@outlook.com>
2025-05-30 22:10:10 -0300

    Prueba 2 Mock

--- src/test/java/com/todocodeacademy/primeraApi/saludoControllerTest.java
+++ src/test/java/com/todocodeacademy/primeraApi/saludoControllerTest.java
@@ -1,5 +1,23 @@
-package com;
+package com.todocodeacademy.primeraApi;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.todocodeacademy.primeraApi.controller.saludoController;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(saludoController.class)
 public class saludoControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
 
+    public void testHolaMundo() throws Exception {
+        mockMvc.perform(get("/apisaludos/hola"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hola Mundo!"));
+    }
 }
